86bb79f953ecb337f720dda361928ceb26db4bb0
Taty5552 <118651490+Taty5552@users.noreply.github.com>
2022-11-20 11:14:09 +0500

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    //привет, хохол
+
+    void Start()
+    {
+        print("Hello World");   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
